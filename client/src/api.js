2394e96e0d830067c04fb700afb2137498c7c563
Sam78Svg <kendresangamwama@gmail.com>
2026-09-14 12:55:36 +0530

    ViteUrlFix

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000' || '';
+const API_URL = import.meta.env.VITE_BACKEND_URL || '';
 console.log(API_URL);
 
 export async function apiFetch(path, options = {}) {
